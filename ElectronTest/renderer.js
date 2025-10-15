1eb1ad1a1bd6e881ceee294616defe12461b139e
HenryNg13111 <nghenry13111@gmail.com>
2025-10-15 15:13:19 -0500

    Step 2 Electron Tutorial

---
+++ ElectronTest/renderer.js
@@ -0,0 +1,8 @@
+const info = document.getElementById("info")
+info.innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+ 
+const func = async ()=>{
+    const response = await window.versions.ping()
+    console.log(response)
+}
+func()
